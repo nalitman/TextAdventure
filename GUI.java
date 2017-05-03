75527260be5ed0520248f7601baeed4357de1adf
nalitman <nalitman17@gmail.com>
2017-05-02 23:21:23 -0500

    How the chuff does one GUI?

--- GUI.java
+++ GUI.java
@@ -1,16 +1,26 @@
-import javax.swing.JFrame;
-import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
 
 public class GUI extends JFrame
 {
-    int W = 400;
-    int H = 300;
-    
     public GUI()
     {
-        setTitle("test thing");
-        setSize(W,H);
-        setVisible(true);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setTitle("Empty Frame");
+        setSize(300,200); // default is 0,0
+        setLocation(10,200); // default is 0,0
+        
+        //Window Listeners
+        addWindowListener(new WindowAdapter(){ public void windowClosing(WindowEvent e)
+                                                { 
+                                                    //Occassionally need dispose();
+                                                    System.exit(0);
+                                                }
+                                            });
+    }
+    
+    public static void main(String[] args)
+    {
+        JFrame f = new GUI();
+        f.show();
     }
 }
---
+++ TestGui.java
@@ -0,0 +1,23 @@
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import java.awt.event.*;
+import java.awt.*;
+import java.util.*;
+
+
+public class TestGui extends JPanel
+{
+    public static void main(String[] args)
+    {
+        JFrame frame = new JFrame();
+        JTextField field = new JTextField();
+        
+        frame.setSize(800,800);
+        field.setSize(200,200);
+        
+        
+        
+    }
+}
