44b65fd79ce92cd6fac3b4b985177a5b24c4e815
nalitman <NALitman@student.naperville203.org>
2017-05-03 15:10:23 -0500

    Player Start

---
+++ Player.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Player
+{
+    private String name; private int x,y,z; private boolean isAlive;
+    private boolean isEquipped; private int health; private Item equippedItem;
+    private ArrayList;
+    
+    public Player(String user)
+    {
+        name = username;
+
+}
